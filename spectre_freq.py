1dde4a8915042662c1b3cf9f88750d01e72db491
Pierre Chapel <pierre.chapel@mines-paristech.fr>
2021-06-11 09:27:50 +0200

    ajout de la visualisation du spectre fréquentiel d'une chronique

--- spectre_freq.py
+++ spectre_freq.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import requests
 import json
 import pandas as pd
@@ -16,36 +15,21 @@ def extract_levels(code_bss):
     doneejson=json.loads(out.text)
     data=pd.json_normalize(doneejson['data'])
     return data[['date_mesure','timestamp_mesure','niveau_nappe_eau','profondeur_nappe']]
-
-#print(extract_levels("04454X0089/PK11.0"))# j'ai entré une aderesse bss de dessus 
+    
+# print(extract_levels("04454X0089/PK11.0"))# j'ai entré une aderesse bss de dessus 
 table = extract_levels("04454X0089/PK11.0")
-# print((table['date_mesure'][1]).split('-')[0])
+#print((table['date_mesure'][1]).split('-')[0])
 
 
 
-def regularize_timestep(table, timestep = 'day', filling_method = 'zero_padding'):
-	# trois timestep aux choix : jour, mois, année. Je sélectionne les valeurs, et en invente si il n'y a pas de données à un endroit.
-	# seule l'option 'day' marche pour l'instant
+def regularize_timestep(table,filling_method = 'zero_padding'):
+	# Je sélectionne les valeurs, et en invente si il n'y a pas de données à un endroit.
+	# méthodes de remplissage : 'zero_padding', 'last_value', 'linear'
 	# la fonction renvoie deux array numpy qui correspondent aux timestamps régularisés et aux profondeurs correspondantes
 	result_time = []
 	result_depths = []
-	
-	if timestep == 'year':
-		counter = 0
-		while counter < len(table['date_mesure']) :
-			year = int( (table['date_mesure'][counter]).split('-')[0] )
-		
-			year, depth, counter = average_year(table,counter,year)
-			result_time.append(year)
-			result_depths.append(depth)
-			
-			while int((table['date_mesure'][counter]).split('-')[0]) != (year + 1):
-				year += 1
-				result_time.append(year)
-				result_depths.append(0)
-		
-		return result_time, result_depths
-	if timestep == 'day':
+
+	if filling_method == 'zero_padding' or filling_method == 'last_value':
 		counter = 0 
 		delta = 86400000 #nbre de ms dans un jour
 		while counter < len(table['date_mesure']) - 1 :
@@ -62,30 +46,60 @@ def regularize_timestep(table, timestep = 'day', filling_method = 'zero_padding'
 				time += delta
 			counter += 1
 		time_array = np.array(result_time)
+		time_array //= delta
 		depth_array = np.array(result_depths)
 		return time_array,depth_array
+	
+	if filling_method == 'linear':
+		counter = 0
+		delta = 86400000
+		temp_depth = []
+		temp_time = []
+		while counter < len(table['date_mesure']) - 1 :
+			time = int( table['timestamp_mesure'][counter] )
+			result_time.append(time)
+			result_depths.append(table['niveau_nappe_eau'][counter])
+			if int( table['timestamp_mesure'][counter+1]) != time + delta:
+				temp_depth=[]
+				temp_time=[]
+				
+				h0 = table['niveau_nappe_eau'][counter]
+				h1 = table['niveau_nappe_eau'][counter + 1]
+				t0 = time
+				t1 = int(table['timestamp_mesure'][counter+1])
+				n = (t1-t0)//delta
+				for i in range(n+1):
+					temp_depth.append(((h1-h0)/n)*i + h0)
+					temp_time.append(t0 + i*delta)
+				result_depths += temp_depth
+				result_time += temp_time
+			counter += 1
+		time_array = np.array(result_time)
+		time_array //= delta
+		depth_array = np.array(result_depths)
+		return time_array,depth_array
+			
+				
+def standardize(depth_array):
+	#renvoie la série de depth_array centrée-réduite pour avoir une meilleure vision fréquentielle. Essentiel pour l'analyse fréquentielle, sinon le pic de valeur moyenn
+	#écrase les pics de variation variationnelle.
+	std = np.std(depth_array)
+	mean = np.mean(depth_array)
+	depth_array -= mean
+	depth_array /= std
+	return depth_array
+					
+				
+result, result1 = regularize_timestep(table,'linear')
 
-
-# def average_year(table,counter,year):
-# 	mean_counter = 1
-# 	S = table['niveau_nappe_eau'][counter]
-# 	counter += 1
-# 	while (table['date_mesure'][counter]).split('-')[0] == year:
-# 		mean_counter += 1
-# 		S += table['niveau_nappe_eau'][counter]
-# 		counter += 1
-# 	depth = (S/mean_counter)
-# 	return year,depth,counter
-
-# print(len(table['date_mesure']))
-result, result1 = regularize_timestep(table,'day', 'last value')
-# plt.scatter(result,result1)
+# plt.scatter(result,standardize(result1))
 # plt.show()
 
 def show_spectrum(array_time, array_depth):
+	array_depth = standardize(array_depth)
 	array_spectrum = np.fft.fft(array_depth)
-	array_freq = np.arange(len(array_time))
-	plt.plot(array_freq,array_spectrum)
+	array_freq = np.fft.fftfreq(array_spectrum.size)
+	plt.plot(array_freq,np.abs(array_spectrum))
 	plt.xlabel("fréquence en jour -1")
 	plt.ylabel("amplitude")
 	plt.show()
